4e20f938cad7672570b83b3a86f63dda7d7749e3
adamcihlar <adam.cihlar3@gmail.com>
2023-10-12 10:33:19 +0200

    Structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ds-template",
+    version="0.0.1",
+    author="Adam Cihlar",
+    author_email="adam.cihlar3@gmail.com",
+    packages=find_packages(),
+)
